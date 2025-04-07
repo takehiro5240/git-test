745ac1539e8946aac61f390ddc6584b6162bda05
takehiro5240 <takeda@gitaku.jp>
2025-04-07 17:39:55 +0900

    第四回のコミット

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,7 +2,7 @@ public class hello {
     public static void main (String[] args){
         System.out.println("hello");
         System.out.println("java");
-        System.out.println("tinko");
+        System.out.println("Hi");
 
     }
 }
